c9da0e16b9bb935dcf1c1cb6701f0c57de4d0d18
Biynazar <bina78@list.ru>
2025-03-27 12:11:10 +0600

    mainLayout, routing, global css

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-import { Counter } from './Counter'
-import List from './List'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { MainLayout } from './components/MainLayout'
 
 function App() {
 
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <hr />
-      <Counter />
-      <List/>
+  // return (
 
-    </>
-  )
+  //   <MainLayout />
+
+  // )
+
+  return <BrowserRouter>
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path='/' element={<div>home</div>} />
+        <Route path='/forbidden' element={<div>💛 forbidden!!!</div>} />
+        <Route path='/addquestion' element={<div>💛 addquestion💛</div>} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App
